143e7066fae23a83582d05c42f2a196bf0372e56
Jesica Rodriguez <jesicarodriguez795@gmil.com>
2023-05-06 00:44:13 -0300

    proyecto

--- javascript/calculotransaccion.js
+++ javascript/calculotransaccion.js
@@ -1,49 +1,55 @@
-document.title="BANCO ALEXA"
+document.title = "BANCO ALEXA";
 
-const valorDolar= 220
-const impuestoPais= 1.30
-const impuestoRg= 1.35
-const input= document.getElementById ("inputDollarAmount")
-const price= document.getElementById ("price")
-const debit= document.getElementById ("debit")
-const amounttaxPais= document.getElementById ("amountTaxPais")
-const amounttaxRg= document.getElementById ("amountTaxRg")
-const total= document.getElementById ("total")
-const taxPais= document.getElementById ("taxPais")
-const taxRg= document.getElementById ("taxRg")
-const accountBalance= document.getElementById ("accountBalance").innerHTML = "$150000.-"
+const impuestoPais = 1.3;
+const impuestoRg = 1.35;
+const input = document.getElementById("inputDollarAmount");
+const debit = document.getElementById("debit");
+const amounttaxPais = document.getElementById("amountTaxPais");
+const amounttaxRg = document.getElementById("amountTaxRg");
+const total = document.getElementById("total");
+const taxPais = document.getElementById("taxPais");
+const taxRg = document.getElementById("taxRg");
+const accountBalance = (document.getElementById("accountBalance").innerHTML =
+  "$150000.-");
 
-const cbu= localStorage.getItem ("cbu")
-document.getElementById ("cbu").innerHTML=cbu
+const cbu = localStorage.getItem("cbu");
+document.getElementById("cbu").innerHTML = cbu;
 
+input.addEventListener("change", async (ev) => {
+  let value = ev.target.value;
+  if (value > 200) {
+    amounttaxPais.innerHTML = " ";
+    amounttaxRg.innerHTML = " ";
+    total.innerHTML = " ";
+    debit.innerHTML = " ";
+    alert("El monto máximo de dolares a comprar es us$200");
+  } else {
+    let importeADebitar = await convertirPesosADolares(value);
+    let calcPais = importeADebitar * impuestoPais - importeADebitar;
+    let calcRg = Number.parseInt(
+      importeADebitar * impuestoRg - importeADebitar
+    );
+    let calcTotal = importeADebitar * impuestoPais * impuestoRg;
+    amounttaxPais.innerHTML = "$" + calcPais.toFixed(2) + "-.";
+    amounttaxRg.innerHTML = "$" + calcRg.toFixed(2) + "-.";
+    total.innerHTML = "$" + calcTotal.toFixed(2) + "-.";
+    debit.innerHTML = "$" + importeADebitar.toFixed(2) + "-.";
+  }
+});
 
-input.addEventListener("change",(ev)=>{
-    let value= ev.target.value
-    console.log(ev.target.value);
-    if (value > 200){
-        amounttaxPais.innerHTML= " "
-        amounttaxRg.innerHTML= " "
-        total.innerHTML= " "
-        debit.innerHTML= " "
-        alert ("El monto máximo de dolares a comprar es us$200")
-        console.log("El monto máximo de dolares a comprar es us$200");
-    }else {
-        let importeADebitar= value*valorDolar
-        let calcPais= (importeADebitar*impuestoPais-importeADebitar)
-        let calcRg= Number.parseInt(importeADebitar*impuestoRg-importeADebitar)
-        let calcTotal= importeADebitar*impuestoPais*impuestoRg
-        amounttaxPais.innerHTML= "$" + calcPais + "-."
-        amounttaxRg.innerHTML= "$" + calcRg + "-."
-        total.innerHTML= "$" + calcTotal+ "-."
-        debit.innerHTML= "$" + importeADebitar + "-."
-        
-        console.log("Importe a debitar: $"+ value*valorDolar);
-        console.log( "$" + (calcPais));
-        console.log("$" + (calcRg));
-        console.log("$" + calcTotal);
-    }
-})
+taxPais.innerHTML = "30%";
+taxRg.innerHTML = "35%";
 
-price.innerHTML= "$" + valorDolar + "-."
-taxPais.innerHTML= "30%"
-taxRg.innerHTML= "35%"
+async function convertirPesosADolares(cantidad) {
+  let resultado;
+  await $.ajax({
+    url:
+      "https://api.exchangerate.host/convert?from=USD&to=ARS&amount=" +
+      cantidad,
+    type: "GET",
+    success(result) {
+      resultado = result.result;
+    },
+  });
+  return resultado;
+}
--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,37 @@
-const datos= {
-  "cbuValido": "01234-54789",
-  "passwordValido": "apocalypse"
-}
+const datos = {
+  cbuValido: "01234-56789",
+  passwordValido: "apocalypse",
+};
 
 function ingresar() {
-  console.log(JSON.stringify(datos))
+  console.log(JSON.stringify(datos));
   const inputCbu = document.getElementById("inputCbu");
   const inputPass = document.getElementById("inputPass");
-  if (inputCbu.value === datos.cbuValido && inputPass.value === datos.passwordValido) {
+  if (
+    inputCbu.value === datos.cbuValido &&
+    inputPass.value === datos.passwordValido
+  ) {
     console.log("cbu o contraseña correcta");
-    localStorage.setItem ("cbu", inputCbu.value)
-    window.location = "./pages/calculotransaccion.html"
+    localStorage.setItem("cbu", inputCbu.value);
+    window.location = "./pages/calculotransaccion.html";
   } else {
     console.log("cbu o contraseña incorrecta");
   }
 }
+
+const cargarImg = (entradas, observador) => {
+  console.log("me estoy ejecutando");
+  console.log(entradas);
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("visible");
+    }
+  });
+};
+const observador = new IntersectionObserver(cargarImg, {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.4,
+});
+
+observador.observe(document.querySelector("#imgCompraWeb"));
